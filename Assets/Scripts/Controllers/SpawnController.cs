fdbe5e054cc1b5ee1286a8b2fc77591f868915c1
Luka Mihaldinec <luka.mihaldinec>
2021-12-30 19:15:39 +0100

    [FIX] Fixed lightnig after reload and fixed rare bug objects falling down

--- Assets/Scripts/Controllers/SpawnController.cs
+++ Assets/Scripts/Controllers/SpawnController.cs
@@ -11,11 +11,11 @@ namespace Smorb.Controllers
 
         public void SpawnObjectToRandomLocation(GameObject prefab)
         {
-            var randomPositionX = Random.Range(1, mapGeneration.MapDepthInTiles * Consts.TILE_SIZE);
-            var randomPositionZ = Random.Range(1, mapGeneration.MapWidthInTiles * Consts.TILE_SIZE);
+            var randomPositionX = Random.Range(Consts.TILE_SIZE / 2, mapGeneration.MapDepthInTiles * Consts.TILE_SIZE - Consts.TILE_SIZE / 2);
+            var randomPositionZ = Random.Range(Consts.TILE_SIZE / 2, mapGeneration.MapWidthInTiles * Consts.TILE_SIZE - Consts.TILE_SIZE / 2);
             var randomPosition = new Vector3(randomPositionX, Consts.MAX_HEIGHT, randomPositionZ);
 
-            var player = Instantiate(prefab, randomPosition, Quaternion.identity, worldTransform);
+            var spawnObj = Instantiate(prefab, randomPosition, Quaternion.identity, worldTransform);
 
             RaycastHit hit;
             if (Physics.Raycast(randomPosition, -Vector3.up, out hit))
@@ -23,13 +23,11 @@ namespace Smorb.Controllers
                 float distanceToGround = hit.distance;
                 if (hit.transform.CompareTag("Ground"))
                 {
-                    player.transform.position = new Vector3(player.transform.position.x,
-                                                            distanceToGround - player.transform.position.y + Consts.SPAWN_OFFSET_Y,
-                                                            player.transform.position.z);
+                    spawnObj.transform.position = new Vector3(spawnObj.transform.position.x,
+                                                            spawnObj.transform.position.y - distanceToGround + Consts.SPAWN_OFFSET_Y,
+                                                            spawnObj.transform.position.z);
                 }
             }
-
-            player.transform.Rotate(Vector3.up * Random.Range(0, 360), Space.World);
         }
     }
 }
